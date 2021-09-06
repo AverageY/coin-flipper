6ba8872282d075a98a7d7ae9827fe315026e5000
AverageY <90152541+AverageY@users.noreply.github.com>
2021-09-06 12:44:52 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,31 +2,27 @@
 
 using namespace std;
 string flip();
-string x;
+char x;
 
 
 int main()
 {
     for(int i=0;i<10;++i){
-    cout<<"Press 1 and enter"<<endl;
-    cin>>x;
-    int z=stoi(x);
-    if(z==1){
-        flip();
-        cout<<"flipped"<<endl;
-
-
-    }
-
-    else {
-        cout<<"try again"<<endl;
-    }
-
-
+        cout<<"Press 1 and enter"<<endl;
+        cin>>x;
+        if((bool)isalpha(x)==true){
+            cout<<"int pls"<<endl;
+        }
+        else if(x=='1'){
+            flip();
+            cout<<"flipped"<<endl;
+        }
+        else {
+            cout<<"try again"<<endl;
+        }
     }
     system("pause>0");
     return 0;
-
 }
 string flip(){
     string ans;
